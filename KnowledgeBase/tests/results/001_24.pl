8153d7fa24444d8b02c7924e20753b5333e15e2e
Minollo <carlo.innocenti@oracle.com>
2023-06-07 16:03:36 -0400

    Additional UTs.

---
+++ KnowledgeBase/tests/results/001_24.pl
@@ -0,0 +1,1 @@
+result(['C1-ABA-CSS-TASK'],[error('202','This Task is not the one expected in this slot by this instance of Technique. Read the Technique card again and make a different choice.')],[],[]).
